f0300e26da9aceec1b9a219c41b18ce18dc9ca3f
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-03-26 16:18:05 -0300

    capitulo 01 completo

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:lojinha_alura/paginas/carrinho.dart';
+import 'package:lojinha_alura/paginas/detalhes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/carrinho': (context) => Carrinho(),
+      },
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatelessWidget {
+  final List moveis = [
+    {
+      "titulo": "Mesa",
+      "preco": 300,
+      "foto": "movel1.jpeg",
+      "descricao":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      "titulo": "Cadeira",
+      "preco": 120,
+      "foto": "movel2.jpg",
+      "descricao":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      "titulo": "Manta",
+      "preco": 200,
+      "foto": "movel3.jpg",
+      "descricao":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      "titulo": "Sofá Cinza",
+      "preco": 800,
+      "foto": "movel4.jpg",
+      "descricao":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      "titulo": "Mesa de cabeceira",
+      "preco": 400,
+      "foto": "movel5.jpg",
+      "descricao":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      "titulo": "Jogo de Cama",
+      "preco": 250,
+      "foto": "movel6.jpg",
+      "descricao":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    },
+    {
+      "titulo": "Sofá Branco",
+      "preco": 900,
+      "foto": "movel7.jpg",
+      "descricao":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: const Text('Inicio'),
+        ),
+      ),
+      body: FlatButton(
+        child: Text('Vamos para os detalhes'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Detalhes(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
